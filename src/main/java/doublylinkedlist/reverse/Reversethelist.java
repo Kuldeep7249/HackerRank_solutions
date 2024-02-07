03bcb156943975dd5e139e41cd19938b1ae3a0ec
Kuldeep7249 <kuldeep304057@gmail.com>
2024-02-07 16:23:10 +0530

    solution of reverse the linked list(hackerrank)

---
+++ src/main/java/doublylinkedlist/reverse/Reversethelist.java
@@ -0,0 +1,138 @@
+package doublylinkedlist.reverse;
+
+
+import java.io.*;
+import java.util.stream.IntStream;
+
+class DoublyLinkedListNode {
+    public int data;
+    public DoublyLinkedListNode next;
+    public DoublyLinkedListNode prev;
+
+    public DoublyLinkedListNode(int nodeData) {
+        this.data = nodeData;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList {
+    public DoublyLinkedListNode head;
+    public DoublyLinkedListNode tail;
+
+    public DoublyLinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void insertNode(int nodeData) {
+        DoublyLinkedListNode node = new DoublyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+            node.prev = this.tail;
+        }
+
+        this.tail = node;
+    }
+}
+
+class DoublyLinkedListPrintHelper {
+    public static void printList(DoublyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+}
+
+class Result {
+
+    /*
+     * Complete the 'reverse' function below.
+     *
+     * The function is expected to return an INTEGER_DOUBLY_LINKED_LIST.
+     * The function accepts INTEGER_DOUBLY_LINKED_LIST llist as parameter.
+     */
+
+    /*
+     * For your reference:
+     *
+     * DoublyLinkedListNode {
+     *     int data;
+     *     DoublyLinkedListNode next;
+     *     DoublyLinkedListNode prev;
+     * }
+     *
+     */
+
+    // public static DoublyLinkedListNode reverse(DoublyLinkedListNode llist) {
+    public static DoublyLinkedListNode reverse(DoublyLinkedListNode head) {
+        DoublyLinkedListNode current = head;
+        DoublyLinkedListNode temp = null;
+
+        // Swap next and prev pointers for all nodes
+        while (current != null) {
+            temp = current.prev;
+            current.prev = current.next;
+            current.next = temp;
+            current = current.prev;
+        }
+
+        // Check if the list is not empty and update the head
+        if (temp != null) {
+            head = temp.prev;
+        }
+
+        return head;
+    }
+
+
+}
+
+ class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                DoublyLinkedList list = new DoublyLinkedList();
+
+                int listCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+                IntStream.range(0, listCount).forEach(i -> {
+                    try {
+                        int listItem = Integer.parseInt(bufferedReader.readLine().trim());
+
+                        list.insertNode(listItem);
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                });
+
+                DoublyLinkedListNode list1 = Result.reverse(list.head);
+
+                DoublyLinkedListPrintHelper.printList(list1, " ", bufferedWriter);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
+
